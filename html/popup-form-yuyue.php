9c0c2fcf1f2744ad772315144cf99d54674fc711
hehui <304574254@qq.com>
2015-08-26 16:09:41 +0800

    popup

--- html/popup-form-yuyue.php
+++ html/popup-form-yuyue.php
@@ -3,33 +3,36 @@
     <div class="form-hd">九正免费定制服务</div>
     <div class="form-bd">
         <form>
-            <div class="input-group input-line">
-                <div class="input-group-addon">您的姓名：</div>
-                <input type="text" class="form-control" id="exampleInputAmount" placeholder="请输入">
-            </div>
-            <div class="input-group input-line">
-                <div class="input-group-addon">您的电话：</div>
-                <input type="text" class="form-control" id="exampleInputAmount" placeholder="请输入">
-            </div>
-            <div class="input-group input-line vcode">
-                <div class="input-group-addon">验证码：</div>
-                <input type="text" class="form-control" id="exampleInputAmount" placeholder="请输入">
-                <div class="input-group-addon">
-                    <img style="width: 90px" src="../img/temp-img/verification-code.png" alt=""/>
+            <div class="form-group">
+                <label for="inputEmail3" class="form-control-label">您的姓名：</label>
+                <div class="input-field">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="请输入">
                 </div>
             </div>
-            <div class="agreement">
-                <label><input type="checkbox"/>我已阅读并同意九正家居网的用户协议</label>
+            <div class="form-group">
+                <label for="inputPassword3" class=" form-control-label">您的电话：</label>
+                <div class="input-field">
+                    <input type="text" class="form-control" id="inputPassword3" placeholder="请输入">
+                </div>
             </div>
-            <div class="">
-                <em>8888</em>人已预定
+            <div class="form-group vcode">
+                <label for="inputPassword3" class=" form-control-label">验证码：</label>
+                <div class="input-field">
+                    <input type="text" class="form-control" id="inputPassword3" placeholder="请输入">
+                    <a href=""><img style="width: 90px" src="../img/temp-img/verification-code.png" alt=""/></a>
+                </div>
             </div>
-            <div class="submit input-line">
-                <button type="button" class="btn btn-danger form-control">免费预约</button>
+            <div class="safety">
+                <label>
+                    <input type="checkbox">我已阅读并同意九正家居网的用户协议
+                </label>
+            </div>
+            <div class="form-group submit">
+                <button type="submit" class="btn btn-danger btn-lg btn-block">免费预约</button>
             </div>
         </form>
     </div>
     <div class="form-ft">
-        免费预约电话：<span><i></i>400-6464-001</span>
+        免费预约电话：<span class="call"><i></i><span>400-6464-001</span>
     </div>
 </div>

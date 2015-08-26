56ce6af8cc094c851a9400eb166fce7e445e2fd8
hehui <304574254@qq.com>
2015-08-27 00:53:56 +0800

    final

--- html/index.php
+++ html/index.php
@@ -89,11 +89,61 @@
                 <p>更专业的装修服务平台，800万业主的共同选择</p>
             </div>
         </div>
+
+<!--        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">-->
+<!--            <ol class="carousel-indicators">-->
+<!--                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>-->
+<!--                <li data-target="#carousel-example-generic" data-slide-to="1"></li>-->
+<!--                <li data-target="#carousel-example-generic" data-slide-to="2"></li>-->
+<!--            </ol>-->
+<!--            <div class="carousel-inner" role="listbox">-->
+<!--                <div class="carousel-item active">-->
+<!--                    <img data-src="holder.js/900x500/auto/#777:#555/text:First slide" alt="First slide">-->
+<!--                </div>-->
+<!--                <div class="carousel-item">-->
+<!--                    <img data-src="holder.js/900x500/auto/#666:#444/text:Second slide" alt="Second slide">-->
+<!--                </div>-->
+<!--                <div class="carousel-item">-->
+<!--                    <img data-src="holder.js/900x500/auto/#555:#333/text:Third slide" alt="Third slide">-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">-->
+<!--                <span class="icon-prev" aria-hidden="true"></span>-->
+<!--                <span class="sr-only">Previous</span>-->
+<!--            </a>-->
+<!--            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">-->
+<!--                <span class="icon-next" aria-hidden="true"></span>-->
+<!--                <span class="sr-only">Next</span>-->
+<!--            </a>-->
+<!--        </div>-->
+
+
         <div class="banner-right-media">
-            <div class="banner-slider">
-                <a style="" href=""><img src="../img/banner1.png" alt=""/></a>
-                <a style="display: none;" href=""><img src="" alt=""/></a>
-                <a style="display: none;" href=""><img src="" alt=""/></a>
+            <div id="carousel-example-generic" class="carousel slide banner-slider" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                        <img src="../img/banner1.png" alt=""/></a>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../img/banner1.png" alt=""/></a>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../img/banner1.png" alt=""/></a>
+                    </div>
+                </div>
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="icon-prev" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="icon-next" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
             <div class="banner-bottom">
                 <a href=""><img src="../img/zuianquan.png" alt=""/></a>

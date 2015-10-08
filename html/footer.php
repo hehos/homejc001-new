86d9295c0f6ce896137f0ccf5098953cff9f7ca4
hehui <304574254@qq.com>
2015-10-09 00:07:41 +0800

    add QQ service

--- html/footer.php
+++ html/footer.php
@@ -20,5 +20,39 @@
     </div>
 </div>
 
+<div id="OICQ" class="fixed-right-OICQ">
+    <div class="OICQ-hd">
+        <div class="OICQ-hd-img">
+            <span class="myicon-QQconsult-title"></span>
+        </div>
+    </div>
+    <div class="OICQ-bd">
+        <ul>
+            <li>
+                客服1 <a class="myicon-QQ-icon"></a>
+            </li>
+            <li>
+                客服2 <a class="myicon-QQ-icon"></a>
+            </li>
+            <li>
+                客服3 <a class="myicon-QQ-icon"></a>
+            </li>
+            <li>
+                客服3 <a class="myicon-QQ-icon"></a>
+            </li>
+            <li>
+                客服3 <a class="myicon-QQ-icon"></a>
+            </li>
+        </ul>
+    </div>
+    <div class="OICQ-ft">
+        <div class="hotline">
+            <div>客服热线</div>
+            <div class="call">400-0129-001</div>
+        </div>
+        <div class="close-row"><a href=""><i>×</i>关闭</a></div>
+    </div>
+</div>
+
 <script src="../js/comm.js"></script>
 <script src="../js/HotSlider.js"></script>
